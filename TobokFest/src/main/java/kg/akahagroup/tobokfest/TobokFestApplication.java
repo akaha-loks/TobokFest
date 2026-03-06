f31f310c4e4f75fb4144b50e66f0759e38628381
Akaha LoKs <132211327+akaha-loks@users.noreply.github.com>
2026-03-06 10:21:37 +0600

    First commit, added project with pom.xml and project structure

---
+++ TobokFest/src/main/java/kg/akahagroup/tobokfest/TobokFestApplication.java
@@ -0,0 +1,13 @@
+package kg.akahagroup.tobokfest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobokFestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TobokFestApplication.class, args);
+    }
+
+}
